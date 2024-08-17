d0d40b34dcdd8ee7e4daef1cb43382f2d79bb30f
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2024-08-18 02:16:16 +1000

    need to properly load in media notes on hover and have to replace all gifs with higher quality videos

---
+++ scripts/scrolling.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var controller = new ScrollMagic.Controller();
+    var scene = new ScrollMagic.Scene({
+        triggerElement: "#trigger1", duration: "200%",
+        triggerHook: 0
+    })
+        .setPin("#media", { pushFollowers: false })
+        .addIndicators({ name: "1 (duration: 300)" }) // add indicators (requires plugin)
+        .addTo(controller);
+})
