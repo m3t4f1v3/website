c0911111c238babea405c5d9ec7b1cb283cde404
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2024-10-16 15:47:56 +0000

    fix inset images and finally use probably correct logic for media background scrolling

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -13,11 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
             trigger: horizontalSection,
             pin: true,
             scrub: 1,
-            snap: 1 / (sections.length - 1), // might be broken
+            snap: {
+                snapTo: 1 / (sections.length - 1),
+                // snapTo: 'labelsDirectional', // snap to the closest label in the timeline
+                duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+                delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+                ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
+            },
             start: "top top",
             end: () => "+=" + (horizontalSection.scrollWidth - window.innerWidth),
-            // markers: { startColor: "white", endColor: "white", fontSize: "18px", fontWeight: "bold", indent: 20 }
+            // markers: true
         }
+    });
+
+    const mediaBackgrounds = gsap.utils.toArray("#media-backgrounds img");
+    animDuration = 1 / (2 * (sections.length - 1)) // magic but seems to work
+    mediaBackgrounds.forEach((mediaBackground, i) => {
+        horizontalTimeline.to(mediaBackground,
+            {
+                opacity: 1,    // Ending state
+                duration: animDuration,
+                ease: "power1.inOut",
+            },
+            (i - 1) * animDuration
+        ).addLabel(mediaBackground)
     });
 
     horizontalTimeline.to(sections, {
@@ -25,39 +44,6 @@ document.addEventListener("DOMContentLoaded", function () {
         ease: 'none',
         // opacity: 1,
     }, 0);
-
-    const mediaBackgrounds = gsap.utils.toArray("#media-backgrounds img");
-
-    // Set up a timeline for the media background images
-    // const mediaBackgroundTimeline = gsap.timeline({
-    //     scrollTrigger: {
-    //         trigger: horizontalSection,
-    //         // pin: true,
-    //         scrub: 1,
-    //         start: "top top",
-    //         end: () => "+=" + (horizontalSection.scrollWidth - window.innerWidth),
-    //         markers: true // Enable markers for debugging
-    //     }
-    // });
-
-    // Add each image to the mediaBackgroundTimeline
-    //https://www.desmos.com/calculator/tx1bxcmvz9
-    // function gaussianDist(x, scale, average, stdev) {
-    //     return scale * Math.exp(-((x - average) ** 2) / (2 * stdev ** 2))
-    // }
-    // let animDuration = 2 * (sections.length);
-
-    // Set up media background opacity transitions
-    // very much broken, cba to fix it though
-    // 12 = 2*sections.length
-    mediaBackgrounds.forEach((mediaBackground, i) => {
-        horizontalTimeline.to(mediaBackground, {
-            opacity: 1,
-            duration: 1 / 12,
-            ease: "power1.inOut",
-        }, Math.max(0, i - 0.5) / 12)
-    });
-
 
     const parallaxImages = gsap.utils.toArray(".parallax-image");
     // Parallax effect for images
@@ -126,7 +112,7 @@ document.addEventListener("DOMContentLoaded", function () {
             start: "center center",
             // end: "center bottom",
             // end: () => "+=" + 2*(eva01.scrollHeight - window.innerHeight), // Ensure full content is scrolled
-            markers: { startColor: "white", endColor: "white", fontSize: "18px", fontWeight: "bold", indent: 20 }
+            // markers: { startColor: "white", endColor: "white", fontSize: "18px", fontWeight: "bold", indent: 20 }
         }
     });
 
